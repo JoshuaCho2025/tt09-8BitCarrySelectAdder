4cd84dda613fd5f8895a73e77ecbfb8645e73945
JoshuaCho2025 <145054637+JoshuaCho2025@users.noreply.github.com>
2024-11-01 13:08:33 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -67,8 +67,21 @@ module MUX2to1_w4(output [3:0] y, input [3:0] i0, i1, input s);
 endmodule
 
 // Carry Select Adder - 8 bits
-module tt_um_CSA8(output [7:0] sum, output cout, input [7:0] a, b);
+module tt_um_CSA8(
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
+  wire [7:0] a, b;
+  wire [7:0] sum;
+  wire cout;
+  
   wire [7:0] sum0, sum1;
   wire c1;
   wire cout0_0, cout0_1;
